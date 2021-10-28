e861acbfe692bdda07ed5bb49ae8d381a1d3c2b2
William <wiliamwijaya1985@gmail.com>
2021-10-28 20:01:21 +0700

    Update readme

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ password: admin123
 
 Untuk mengakses URI `/table` dan `/graph`, pengguna harus login terlebih dahulu.  
 Jika pengguna sudah login, maka pengguna langsung diarahkan ke URI `/table`.
+
+## Requirements
+1. Import `pweb-1.sql` ke database MySQL local.
+2. Pada folder `database`, copy file `config.example.php` menjadi `config.php`.
+3. Isi file `config.php` sesuai dengan konfigurasi database.
--- database/config.php
+++ database/config.php
@@ -3,10 +3,10 @@
 server with default setting (user 'root' with no password) */
 $config = [
     'database' => [
-        'name' => 'sql313.epizy.com',
-        'username' => 'epiz_30202296',
-        'password' => 'ml9uOP1pHQD2',
-        'connection' => 'epiz_30202296_pweb',
+        'name' => 'pweb-1',
+        'username' => 'root',
+        'password' => '',
+        'connection' => 'localhost',
         'options' => []
     ]
 ];
