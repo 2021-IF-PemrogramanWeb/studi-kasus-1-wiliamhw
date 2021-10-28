eefee0022173379d35e66b62afacb7d2833dc46e
William <wiliamwijaya1985@gmail.com>
2021-10-28 16:09:23 +0700

    Adjust login view

---
+++ database/config.php
@@ -0,0 +1,12 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$config = [
+    'database' => [
+        'name' => 'pendaftaran_siswa',
+        'username' => 'root',
+        'password' => '',
+        'connection' => 'localhost',
+        'options' => []
+    ]
+];
